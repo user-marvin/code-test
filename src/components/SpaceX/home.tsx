b37b855213236e84ffb13c16ec65a679871c4c45
Marvin Villamar <villamar.marvin.b.8138gmail.com@Marvins-MacBook-Pro.local>
2025-03-12 13:19:13 +0800

    lazy loading

--- src/components/SpaceX/home.tsx
+++ src/components/SpaceX/home.tsx
@@ -11,7 +11,7 @@ const Home: React.FC = () => {
   const [spacex, setSpacex] = useState<LaunchDetails[]>([]);
   const [visibleData, setVisibleData] = useState<LaunchDetails[]>([]);
   const [page, setPage] = useState<number>(1);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const loader = useRef<HTMLDivElement | null>(null);
 
@@ -19,8 +19,8 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const data = await fetchApi();
-      console.log(data);
       setSpacex(data);
       setVisibleData(data.slice(0, ITEMS_PER_PAGE));
       setLoading(false);
@@ -28,7 +28,34 @@ const Home: React.FC = () => {
     fetchData();
   }, []);
 
+  const filteredData = searchTerm
+    ? spacex.filter((item) =>
+        item.mission_name.toLowerCase().includes(searchTerm)
+      )
+    : spacex;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.toLowerCase();
+    setSearchTerm(value);
+    setPage(1);
+    setVisibleData(filteredData.slice(0, ITEMS_PER_PAGE));
+  };
+
+  const loadMoreItems = () => {
+    const nextPage = page + 1;
+    const start = (nextPage - 1) * ITEMS_PER_PAGE;
+    const end = start + ITEMS_PER_PAGE;
+    const moreItems = filteredData.slice(start, end);
+
+    if (moreItems.length > 0) {
+      setVisibleData((prev) => [...prev, ...moreItems]);
+      setPage(nextPage);
+    }
+  };
+
   useEffect(() => {
+    if (searchTerm) return;
+
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting && !loading) {
@@ -43,82 +70,55 @@ const Home: React.FC = () => {
     return () => {
       if (loader.current) observer.unobserve(loader.current);
     };
-  }, [loader, loading, visibleData]);
-
-  const filteredData = searchTerm
-    ? spacex.filter((item) =>
-        item.mission_name.toLowerCase().includes(searchTerm)
-      )
-    : spacex;
-
-  const loadMoreItems = () => {
-    setLoading(true);
-    const nextPage = page + 1;
-    const start = (nextPage - 1) * ITEMS_PER_PAGE;
-    const end = start + ITEMS_PER_PAGE;
-    const moreItems = filteredData.slice(start, end);
-
-    if (moreItems.length > 0) {
-      setVisibleData((prev) => [...prev, ...moreItems]);
-      setPage(nextPage);
-    }
-
-    setLoading(false);
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value.toLowerCase();
-    setSearchTerm(value);
-    setPage(1);
-    const filtered = spacex.filter((item) =>
-      item.mission_name.toLowerCase().includes(value)
-    );
-    setVisibleData(filtered.slice(0, ITEMS_PER_PAGE)); // First page of search result
-  };
+  }, [loader, loading, filteredData, page, searchTerm]);
 
   return (
-    <Grid className="h-[90vh] w-[500px] overflow-auto scrollbar-hide">
-      <Grid size={12}>
-        <div className="m-2">
-          <TextField
-            onChange={handleChange}
-            id="outlined-basic"
-            label="Search..."
-            variant="outlined"
-            sx={{
-              width: "100%",
-              "& .MuiOutlinedInput-root": {
-                "& fieldset": {
-                  borderColor: "white",
-                },
+    <>
+      <div className="mb-2">
+        <TextField
+          onChange={handleChange}
+          id="outlined-basic"
+          label="Search..."
+          variant="outlined"
+          sx={{
+            width: "100%",
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "white",
               },
-              input: {
-                color: "white",
-              },
-              label: {
-                color: "white",
-              },
-            }}
-          />
-        </div>
-        <Suspense fallback={<Spinner />}>
-          {visibleData.map((item) => (
-            <LaunchItem key={item.flight_number} launch={item} />
-          ))}
-        </Suspense>
-      </Grid>
-
-      <div
-        ref={loader}
-        className="h-10 w-full flex justify-center items-center"
-      >
-        {loading ? (
-          <Spinner />
-        ) : visibleData.length >= filteredData.length ? (
-          <span className="text-white">End of List</span>
-        ) : null}
+            },
+            input: {
+              color: "white",
+            },
+            label: {
+              color: "white",
+            },
+          }}
+        />
       </div>
-    </Grid>
+      <Grid className="h-[90vh] w-[500px] overflow-auto scrollbar-hide top-5">
+        <Grid size={12}>
+          <Suspense fallback={<Spinner />}>
+            {visibleData.map((item) => (
+              <LaunchItem key={item.flight_number} launch={item} />
+            ))}
+          </Suspense>
+        </Grid>
+
+        <div
+          ref={loader}
+          className="h-10 w-full flex justify-center items-center"
+        >
+          {loading ? (
+            <Spinner />
+          ) : visibleData.length >= filteredData.length && !searchTerm ? (
+            <span className="text-white">End of List</span>
+          ) : (
+            !searchTerm && <Spinner />
+          )}
+        </div>
+      </Grid>
+    </>
   );
 };
 
